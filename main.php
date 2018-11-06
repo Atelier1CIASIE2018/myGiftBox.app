cd093594c2f9098448dad6367c0bc5d9bdb05290
Louis Maillard <l.maillard55000@gmail.com>
2018-11-06 09:33:56 +0100

    création routes

--- main.php
+++ main.php
@@ -26,9 +26,23 @@ $db->bootEloquent();           /* établir la connexion */
 
 $router = new \mf\router\Router();
 
-$router->addRoute('home', '/home/', '\giftbox\control\mygiftboxController', 'viewHome');
-
-$router->addRoute('prestation', '/prestation/', '\giftbox\control\mygiftboxController', 'viewPrestation');
+$router->addRoute('home', '/home/', '\giftbox\control\GiftboxController', 'viewHome'); //GET
+$router->addRoute('prestations', '/prestations/', '\giftbox\control\GiftboxController', 'viewPrestations'); //GET
+$router->addRoute('prestation', '/prestation/', '\giftbox\control\GiftboxController', 'viewPrestation'); //GET
+$router->addRoute('categories', '/categories/', '\giftbox\control\GiftboxController', 'viewCategories'); //GET
+$router->addRoute('categorie', '/categorie/', '\giftbox\control\GiftboxController', 'viewCategorie'); //GET
+$router->addRoute('login', '/login/', '\giftbox\control\GiftboxController', 'viewLogin'); //GET
+$router->addRoute('loginPost', '/loginPost/', '\giftbox\control\GiftboxController', 'postLogin'); //POST
+$router->addRoute('register', '/register/', '\giftbox\control\GiftboxController', 'viewRegister'); //GET
+$router->addRoute('registerPost', '/registerPost/', '\giftbox\control\GiftboxController', 'postRegister'); //POST
+$router->addRoute('box', '/box/', '\giftbox\control\GiftboxController', 'viewBox'); //GET
+$router->addRoute('newBox', '/box/new/', '\giftbox\control\GiftboxController', 'newBox'); //GET
+$router->addRoute('postBox', '/box/post/', '\giftbox\control\GiftboxController', 'postBox'); //POST
+$router->addRoute('addBox', '/box/add/', '\giftbox\control\GiftboxController', 'addBox'); //GET
+$router->addRoute('removeBox', '/box/remove/', '\giftbox\control\GiftboxController', 'removeBox'); //GET
+$router->addRoute('updateBox', '/box/update/', '\giftbox\control\GiftboxController', 'updateBox'); //UPDATE
+$router->addRoute('profile', '/profile/', '\giftbox\control\GiftboxController', 'viewProfile'); //GET
+$router->addRoute('profileUpdate', '/profile/update/', '\giftbox\control\GiftboxController', ''); //GET
 
 $router->setDefaultRoute('/home/');
 
