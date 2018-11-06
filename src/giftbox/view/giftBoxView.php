4a38cab3aa917e1215b73aa2499cbb36236d4185
Yvann Toussaint <yvann.om@orange.fr>
2018-11-06 16:33:51 +0100

    render Categ

--- src/giftbox/view/giftBoxView.php
+++ src/giftbox/view/giftBoxView.php
@@ -47,7 +47,6 @@ class GiftBoxView extends \mf\view\AbstractView {
     private function renderPrestations(){
         $res = "<div>";
         foreach ($this->data as $value1) {
-            $router = new \mf\router\Router();
             $res = $res . "<div>
                 <p>".$value1['Nom']."</p>
             </div>";
@@ -55,16 +54,50 @@ class GiftBoxView extends \mf\view\AbstractView {
         $res = $res . "</div>";
         return $res;
     }
+
+    private function renderCategories(){
+        $res = "<div>";
+        //var_dump($this->data);
+        foreach ($this->data as $value) {
+            $res = $res."<div> <p>".$value['Nom']."</p> </div>";
+        }
+        $res = $res."</div>";
+        return $res;
+    }
+
+    private function renderCategorie(){
+        $res = "<div>";
+        //var_dump($this->data);
+        foreach ($this->data as $value1) {
+
+            $c = \giftbox\model\Prestation::where('Id' ,'=', $value1['Id'])->first();
+
+            $router = new \mf\router\Router();
+            $urlCateg = $router->urlfor('/prestation/', ['Id'=>$value1['Id']]);
+
+            //var_dump($value1);
+            $res = $res . "<a href='".$urlCateg."'><div>
+                <p>".$value1['Nom']."</p>
+                <p>".$value1['Prix']." €</p>
+                <img src ='/giftBox/img/".$value1['Img']."' width='200'>
+                <p>".$value1['Description']."</p>
+            </div></a>";
+        }
+        $res = $res."</div>";
+        return $res;
+    }
+
+    
     
     protected function renderBody($selector=null){
 
         $string = "<header>".self::renderHeader()."</header><section><article>";
-        if ($selector == 'home')$string = $string . self::renderHome();
+        if ($selector == 'Home')$string = $string . self::renderHome();
         if ($selector == 'Prestation')$string = $string . self::renderPrestation();
         if ($selector == 'Prestations')$string = $string . self::renderPrestations();
+        if ($selector == 'Categories')$string = $string . self::renderCategories();
+        if ($selector == 'Categorie')$string = $string . self::renderCategorie();
         /*if ($selector == '')$string = $string . self::();
-        if ($selector == '')$string = $string . self::();
-        if ($selector == '')$string = $string . self::();
         if ($selector == '')$string = $string . self::();
         if ($selector == '')$string = $string . self::();
         if ($selector == '')$string = $string . self::();
